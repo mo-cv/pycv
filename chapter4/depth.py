1455b0742586c2a112b73954a01676378324c788
Dongsong <dongsong.mo@thistech.com>
2017-09-21 17:22:25 +0800

    Update:

--- chapter4/depth.py
+++ chapter4/depth.py
@@ -2,84 +2,99 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-def to_uint8( data ) :
+
+def createMedianMask(disparityMap, validDepthMask, rect=None):
+    """Return a mask selecting the median layer, plus shadows."""
+    if rect is not None:
+        x, y, w, h = rect
+        disparityMap = disparityMap[y:y + h, x:x + w]
+        validDepthMask = validDepthMask[y:y + h, x:x + w]
+    median = np.median(disparityMap)
+    return np.where((validDepthMask == 0) |
+                       (abs(disparityMap - median) < 12),
+                       1.0, 0.0)
+
+def to_uint8(data):
     # maximum pixel
-    latch = np.zeros_like( data )
+    latch = np.zeros_like(data)
     latch[:] = 255
     # minimum pixel
-    zeros = np.zeros_like( data )
+    zeros = np.zeros_like(data)
 
     # unrolled to illustrate steps
-    d = np.maximum( zeros, data )
-    d = np.minimum( latch, d )
+    d = np.maximum(zeros, data)
+    d = np.minimum(latch, d)
 
     # cast to uint8
-    return np.asarray( d, dtype="uint8" )
+    return np.asarray(d, dtype="uint8")
 
-def drawlines(img1,img2,lines,pts1,pts2):
-  ''' img1 - image on which we draw the epilines for the points in img2
-      lines - corresponding epilines '''
-  print img1.shape
-  r,c, ch = img1.shape
-  clr1 = cv2.pyrDown(cv2.imread('./images/stacked1.png', 0))
-  clr2 = cv2.pyrDown(cv2.imread('./images/stacked2.png', 0))
-  for r,pt1,pt2 in zip(lines,pts1,pts2):
-      color = tuple(np.random.randint(0,255,3).tolist())
-      x0,y0 = map(int, [0, -r[2]/r[1] ])
-      x1,y1 = map(int, [c, -(r[2]+r[0]*c)/r[1] ])
-      clr1 = cv2.line(clr1, (x0,y0), (x1,y1), color,1)
-      clr1 = cv2.circle(clr1,tuple(pt1),5,color,-1)
-      clr2 = cv2.circle(clr2,tuple(pt2),5,color,-1)
-      return clr1, clr2
 
-img1 = to_uint8(cv2.pyrDown(cv2.imread('./images/stacked1.png', cv2.COLOR_BGR2GRAY)))  #queryimage # left image
-img2 = to_uint8(cv2.pyrDown(cv2.imread('./images/stacked2.png', cv2.COLOR_BGR2GRAY))) #trainimage # right image
+def drawlines(img1, img2, lines, pts1, pts2):
+    ''' img1 - image on which we draw the epilines for the points in img2
+        lines - corresponding epilines '''
+    print img1.shape
+    r, c, ch = img1.shape
+    clr1 = cv2.pyrDown(cv2.imread('./images/stacked1.png', 0))
+    clr2 = cv2.pyrDown(cv2.imread('./images/stacked2.png', 0))
+    for r, pt1, pt2 in zip(lines, pts1, pts2):
+        color = tuple(np.random.randint(0, 255, 3).tolist())
+        x0, y0 = map(int, [0, -r[2] / r[1]])
+        x1, y1 = map(int, [c, -(r[2] + r[0] * c) / r[1]])
+        clr1 = cv2.line(clr1, (x0, y0), (x1, y1), color, 1)
+        clr1 = cv2.circle(clr1, tuple(pt1), 5, color, -1)
+        clr2 = cv2.circle(clr2, tuple(pt2), 5, color, -1)
+        return clr1, clr2
+
+img1 = to_uint8(cv2.pyrDown(cv2.imread('./images/stacked1.png',
+                                       cv2.COLOR_BGR2GRAY)))  # queryimage # left image
+img2 = to_uint8(cv2.pyrDown(cv2.imread('./images/stacked2.png',
+                                       cv2.COLOR_BGR2GRAY)))  # trainimage # right image
 
 sift = cv2.xfeatures2d.SIFT_create()
- 
+
 # find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
+kp1, des1 = sift.detectAndCompute(img1, None)
+kp2, des2 = sift.detectAndCompute(img2, None)
 
 # FLANN parameters
 FLANN_INDEX_KDTREE = 0
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
 search_params = dict(checks=50)
- 
-flann = cv2.FlannBasedMatcher(index_params,search_params)
-matches = flann.knnMatch(des1,des2,k=2)
- 
+
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+matches = flann.knnMatch(des1, des2, k=2)
+
 good = []
 pts1 = []
 pts2 = []
- 
+
 # ratio test as per Lowe's paper
-for i,(m,n) in enumerate(matches):
-    if m.distance < 0.8*n.distance:
+for i, (m, n) in enumerate(matches):
+    if m.distance < 0.8 * n.distance:
         good.append(m)
         pts2.append(kp2[m.trainIdx].pt)
         pts1.append(kp1[m.queryIdx].pt)
 
 pts1 = np.int32(pts1)
 pts2 = np.int32(pts2)
-F, mask = cv2.findFundamentalMat(pts1,pts2,cv2.FM_LMEDS)
- 
+F, mask = cv2.findFundamentalMat(pts1, pts2, cv2.FM_LMEDS)
+
 # We select only inlier points
-pts1 = pts1[mask.ravel()==1]
-pts2 = pts2[mask.ravel()==1]
+pts1 = pts1[mask.ravel() == 1]
+pts2 = pts2[mask.ravel() == 1]
 
 # Find epilines corresponding to points in right image (second image) and
 # drawing its lines on left image
-lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1,1,2), 2,F)
-lines1 = lines1.reshape(-1,3)
-img5,img6 = drawlines(img1,img2,lines1,pts1,pts2)
+lines1 = cv2.computeCorrespondEpilines(pts2.reshape(-1, 1, 2), 2, F)
+lines1 = lines1.reshape(-1, 3)
+img5, img6 = drawlines(img1, img2, lines1, pts1, pts2)
 
 # Find epilines corresponding to points in left image (first image) and
 # drawing its lines on right image
-lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1,1,2), 1,F)
-lines2 = lines2.reshape(-1,3)
-img3,img4 = drawlines(img2,img1,lines2,pts2,pts1)
+lines2 = cv2.computeCorrespondEpilines(pts1.reshape(-1, 1, 2), 1, F)
+lines2 = lines2.reshape(-1, 3)
+img3, img4 = drawlines(img2, img1, lines2, pts2, pts1)
 
-plt.subplot(121),plt.imshow(img5)
-plt.subplot(122),plt.imshow(img3)
+plt.subplot(121), plt.imshow(img5)
+plt.subplot(122), plt.imshow(img3)
 plt.show()
--- chapter4/watershed.py
+++ chapter4/watershed.py
@@ -2,9 +2,9 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('images/basil.jpg')
+img = cv2.imread('../images/basil.jpg')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+ret1, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 # noise removal
 kernel = np.ones((3,3),np.uint8)
 opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
@@ -14,14 +14,14 @@ sure_bg = cv2.dilate(opening,kernel,iterations=3)
 
 # Finding sure foreground area
 dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
-ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+ret2, sure_fg = cv2.threshold(dist_transform,0.5*dist_transform.max(),255,0)
 
 # Finding unknown region
 sure_fg = np.uint8(sure_fg)
 unknown = cv2.subtract(sure_bg,sure_fg)
 
 # Marker labelling
-ret, markers = cv2.connectedComponents(sure_fg)
+ret3, markers = cv2.connectedComponents(sure_fg)
 
 # Add one to all labels so that sure background is not 0, but 1
 markers = markers+1
@@ -31,5 +31,22 @@ markers[unknown==255] = 0
 markers = cv2.watershed(img,markers)
 img[markers == -1] = [255,0,0]
 
-plt.imshow(img)
+plt.subplot(231), plt.imshow(thresh)
+plt.title("thresh"), plt.xticks([]), plt.yticks([])
+
+plt.subplot(232), plt.imshow(opening)
+plt.title("opening"), plt.xticks([]), plt.yticks([])
+
+plt.subplot(233), plt.imshow(sure_bg)
+plt.title("sure_bg"), plt.xticks([]), plt.yticks([])
+
+plt.subplot(234), plt.imshow(dist_transform)
+plt.title("dist_transform"), plt.xticks([]), plt.yticks([])
+
+plt.subplot(235), plt.imshow(markers)
+plt.title("markers"), plt.xticks([]), plt.yticks([])
+
+plt.subplot(236), plt.imshow(img)
+plt.title("opening"), plt.xticks([]), plt.yticks([])
+
 plt.show()
